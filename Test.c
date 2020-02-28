2130446e465f4d4383938c1de7503af368b0d4a1
thanhdzaz <61109690+thanhdzaz@users.noreply.github.com>
2020-02-28 11:16:57 +0700

    Create Test.c

---
+++ Test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  float a,b,c;
+  printf("nhap a");
+  scanf("%f",&a);
+  printf("nhap b");
+  scanf("%f",&b);
+  printf("nhap c");
+  scanf("%f",&c);
+  float tb;
+  tb = (a+b+c)/3;
+  printf("trung binh = %f",tb);
+}
